edef073b8a31c30696d4d90982d82bc550bc70ce
Daniel Cazzulino <daniel@cazzulino.com>
2018-02-17 10:17:32 -0300

    Update corebuild, generate and upload binary logs

--- build.cmd
+++ build.cmd
@@ -1,24 +1,31 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Perf/usability tweaks for MSBuild
+set MSBUILDLOGASYNC=1
+set MSBUILDLOGTASKINPUTS=1
+set MSBUILDDISABLENODEREUSE=1
+
 set BatchFile=%0
 set Root=%~dp0
-set BuildConfiguration=Debug
-set MSBuildTarget=All
 set NodeReuse=true
 set MultiProcessor=/m
-set MSBuildAdditionalArguments=/m
+set BuildConfiguration=/t:Debug
+set MSBuildTarget=/t:All
+set MSBuildAdditionalArguments=
 
 :ParseArguments
 if "%1" == "" goto :DoneParsing
 if /I "%1" == "/?" call :Usage && exit /b 1
 if /I "%1" == "/debug" set BuildConfiguration=Debug&&shift&& goto :ParseArguments
 if /I "%1" == "/release" set BuildConfiguration=Release&&shift&& goto :ParseArguments
-if /I "%1" == "/build" set MSBuildTarget=Build&&shift&& goto :ParseArguments
-if /I "%1" == "/rebuild" set MSBuildTarget=Rebuild&&shift&& goto :ParseArguments
-if /I "%1" == "/package" set MSBuildTarget=Package&&shift&& goto :ParseArguments
-if /I "%1" == "/test" set MSBuildTarget=Test&&shift&& goto :ParseArguments
-if /I "%1" == "/restore" set MSBuildTarget=Restore&&shift&& goto :ParseArguments
+if /I "%1" == "/build" set MSBuildTarget=/t:Build&&shift&& goto :ParseArguments
+if /I "%1" == "/clean" set MSBuildTarget=/t:Clean&&shift&& goto :ParseArguments
+if /I "%1" == "/rebuild" set MSBuildTarget=/t:Rebuild&&shift&& goto :ParseArguments
+if /I "%1" == "/package" set MSBuildTarget=/t:Package&&shift&& goto :ParseArguments
+if /I "%1" == "/restore" set MSBuildTarget=/t:Restore&&shift&& goto :ParseArguments
+if /I "%1" == "/update" set MSBuildTarget=/t:Update&&shift&& goto :ParseArguments
+if /I "%1" == "/test" set MSBuildTarget=/t:Test&&shift&& goto :ParseArguments
 if /I "%1" == "/no-node-reuse" set NodeReuse=false&&shift&& goto :ParseArguments
 if /I "%1" == "/no-multi-proc" set MultiProcessor=&&shift&& goto :ParseArguments
 set MSBuildAdditionalArguments=%MSBuildAdditionalArguments% %1&&shift&& goto :ParseArguments
@@ -59,7 +66,7 @@ if %MSBuildMajorVersion% LSS 15 (
 
 :: Ensure developer command prompt variables are set
 if "%VisualStudioVersion%" == "" (
-  for /f "delims=" %%i in ('msbuild build.props -nologo /t:GetVsInstallRoot') do set "VsInstallRoot=%%i" & goto :VsInstallRootDone
+  for /f "delims=" %%i in ('msbuild -nologo -noautoresponse %Root%build.proj /t:_GetVsInstallRoot /v:minimal') do set "VsInstallRoot=%%i" & goto :VsInstallRootDone
 :VsInstallRootDone
   for /f "tokens=* delims= " %%i in ("%VsInstallRoot%") do set "VsInstallRoot=%%i"
   set "DeveloperCommandPrompt=%VsInstallRoot%\Common7\Tools\VsDevCmd.bat"
@@ -75,8 +82,10 @@ if "%VisualStudioVersion%" == "" (
   call "%DeveloperCommandPrompt%" || goto :BuildFailed
 )
 
+if not "%MSBuildTarget%" == "" set MSBuildTargetName=%MSBuildTarget:~3%
+
 @echo on
-msbuild /nologo /nodeReuse:%NodeReuse% /t:"%MSBuildTarget%" /p:target="%MSBuildTarget%" /p:Configuration="%BuildConfiguration%" %MSBuildAdditionalArguments%
+msbuild "%Root%build.proj" /nologo /nr:%NodeReuse% %MultiProcessor% %MSBuildTarget% /p:target=%MSBuildTargetName% /p:Configuration=%BuildConfiguration% %MSBuildAdditionalArguments%
 @echo off
 if ERRORLEVEL 1 (
     echo.
@@ -85,25 +94,30 @@ if ERRORLEVEL 1 (
 )
 
 echo.
-call :PrintColor Green "Build completed successfully, for full log see msbuild.log"
+call :PrintColor Cyan "Build completed successfully, for full log see msbuild.log"
 exit /b 0
 
 :Usage
-echo Usage: %BatchFile% [/build^|/rebuild^|/test^|/restore^] [/debug^|/release] [/no-node-reuse] [/no-multi-proc]
+echo Usage: %BatchFile% [/build^|/clean^|/rebuild^|/restore^|/update^|/test^] [/debug^|/release] [/no-node-reuse] [/no-multi-proc] [OPTIONS]
 echo.
 echo   Build targets:
-echo     /build                   Perform build
-echo     /rebuild                 Perform a clean, then build
-echo     /package                 Packages the product
-echo     /test                    Builds and runs tests only
+echo     /build                   Builds the project
+echo     /clean                   Cleans a previous build
+echo     /rebuild                 Cleans, then builds
 echo     /restore                 Only restore NuGet packages
+echo     /update                  Updates corebuild targets and dependencies
+echo     /package                 Packages the product
+echo     /test                    Runs tests
 echo.
 echo   Build options:
-echo     /debug                   Perform debug build (default)
-echo     /release                 Perform release build
+echo     /debug                   Perform debug build (default, /p:Configuration=Debug)
+echo     /release                 Perform release build (/p:Configuration=Release)
 echo     /no-node-reuse           Prevents MSBuild from reusing existing MSBuild instances,
-echo                              useful for avoiding unexpected behavior on build machines
-echo     /no-multi-proc           No multi-proc build, useful for diagnosing build logs
+echo                              useful for avoiding unexpected behavior on build machines ('/nr:false' switch)
+echo     /no-multi-proc           No multi-proc build, useful for diagnosing build logs (no '/m' switch)
+echo     /noautoresponse          Do not process the msbuild.rsp response file options
+echo
+echo     [OPTIONS]                Arbitrary MSBuild switches can also be passed in.
 goto :eof
 
 :BuildFailed
